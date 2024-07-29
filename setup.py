f8bedc6e502ae69f9ec0b15aea444039ab7a4e50
Petr Jasek <jasekpetr@gmail.com>
2024-07-29 11:50:36 +0200

    fix missing import in setuptools

--- setup.py
+++ setup.py
@@ -1,31 +1,12 @@
 __version__ = '1.3.4'
 
 import os
-import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ['src/test_mongolock.py']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setup(
     name='mongolock',
@@ -48,10 +29,7 @@ setup(
     license="BSD",
     package_dir={'': 'src'},
     py_modules=['mongolock'],
-    cmdclass={'test': PyTest},
-    test_suite="test_mongolock",
     include_package_data=True,
     zip_safe=False,
-    tests_require=['pytest>=2.6.0'],
     install_requires=["pymongo>=2.6.0"]
 )
